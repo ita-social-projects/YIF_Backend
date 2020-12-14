6b1cd900befbd4f75b02692654c4015acc6934ea
Anton Menchinskii <anton.menchinskii@gmail.com>
2020-12-14 11:43:02 +0200

    Added UsersControllerTests

--- YIF_XUnitTests/Integration/YIF_Backend/Controllers/UsersControllerTests.cs
+++ YIF_XUnitTests/Integration/YIF_Backend/Controllers/UsersControllerTests.cs
@@ -13,25 +13,52 @@ namespace YIF_XUnitTests.Integration.YIF_Backend.Controllers
         : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly HttpClient _client;
-        private readonly string _root;
         public UsersControllerTests()
         {
+            var clientOptions = new WebApplicationFactoryClientOptions();
+            clientOptions.BaseAddress = new Uri("https://localhost:44324/api/Users/");
+
             var appFactory = new WebApplicationFactory<Startup>();
-            _client = appFactory.CreateClient();
-            _root = "https://localhost:44324/api/Users";
+            _client = appFactory.CreateClient(clientOptions);
         }
 
         [Theory]
-        [InlineData("/")]
-        [InlineData("/00f5e3b3-fa90-4e74-856f-cbd41f178529")]
-        public async Task GetUserTest(string url)
+        [InlineData("")]
+        [InlineData("00f5e3b3-fa90-4e74-856f-cbd41f178529")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
         {
             // Act            
-            var response = await _client.GetAsync(_root + url);
-
+            var response = await _client.GetAsync(url);
+            
             // Assert
             response.EnsureSuccessStatusCode();
             Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("00f5e3b3-fa90-4e74-856f-cbd41f178520")]
+        public async Task Get_EndpointReturnNotFound(string url)
+        {
+            // Act            
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+            Assert.Equal("text/plain; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("d")]
+        public async Task Get_EndpointReturnBadRequest(string url)
+        {
+            // Act            
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Equal("text/plain; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
     }
