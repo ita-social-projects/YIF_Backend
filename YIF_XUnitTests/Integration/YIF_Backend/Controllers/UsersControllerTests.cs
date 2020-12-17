e3926f375ee9dae133470be353da0a50451db9ee
Kolia <kolyalyutko@gmail.com>
2020-12-17 11:34:57 +0200

    Fix InlineData in integration test

--- YIF_XUnitTests/Integration/YIF_Backend/Controllers/UsersControllerTests.cs
+++ YIF_XUnitTests/Integration/YIF_Backend/Controllers/UsersControllerTests.cs
@@ -22,7 +22,7 @@ namespace YIF_XUnitTests.Integration.YIF_Backend.Controllers
 
         [Theory]
         [InlineData("")]
-        [InlineData("00f5e3b3-fa90-4e74-856f-cbd41f178529")]
+        [InlineData("117ab34c-7bb7-4526-b84e-069b476a6279")]
         public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
         {
             // Act            
