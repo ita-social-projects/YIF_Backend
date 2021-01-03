484f52bbdc6556241438e4f496c2c3bde422a3ef
smetanskyy <stepansmetanskyy@gmail>
2021-01-03 19:58:18 +0200

    add SendGridService

---
+++ YIF.Core.Domain/ServiceInterfaces/ISendGridService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace YIF.Core.Domain.ServiceInterfaces
+{
+    public interface IEmailService
+    {
+        Task<bool> Send(string email, string subject, string plainTextContent)
+    }
+}
---
+++ YIF.Core.Service/Concrete/Services/SendGridService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+using YIF.Core.Domain.ServiceInterfaces;
+
+namespace YIF.Core.Service.Concrete.Services
+{
+    public class SendGridService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _apiKey;
+        private readonly string _fromMail;
+
+        public SendGridService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _apiKey = _configuration.GetValue<string>("SendGrid:SendGridApi");
+            _fromMail = _configuration.GetValue<string>("SendGrid:SendGridMail");
+        }
+        public async Task<bool> Send(string email, string subject, string plainTextContent)
+        {
+            var client = new SendGridClient(_apiKey);
+            var from = new EmailAddress(_fromMail, _fromMail);
+            subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress(email, email);
+            plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
