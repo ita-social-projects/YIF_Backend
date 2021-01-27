f470ac26d00549678e619ebcf5ef60a11012cc04
Bandera <42476974+Bandera1@users.noreply.github.com>
2021-01-27 22:42:03 +0200

    Fix university integration tests

--- YIF_XUnitTests/Integration/YIF_Backend/Controllers/UniversityControllerTests.cs
+++ YIF_XUnitTests/Integration/YIF_Backend/Controllers/UniversityControllerTests.cs
@@ -30,7 +30,7 @@ namespace YIF_XUnitTests.Integration.YIF_Backend.Controllers
         #region CorrectTests
 
         [Theory]
-        [InlineData("%D0%86%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D1%96%D0%B9%D0%BD%D1%96%20%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D1%96%D1%97")]
+        [InlineData("%D0%9C%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0%20%D1%82%D0%B0%20%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D0%BA%D0%B0")]
         public async Task GET_EndpointsReturnUniversities_IfDirectionNameCorrect(string DirectionName)
         {
             // Act
@@ -49,7 +49,7 @@ namespace YIF_XUnitTests.Integration.YIF_Backend.Controllers
         }
 
         [Theory]
-        [InlineData("%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%BD%D0%B8%D0%B9%20%D0%B0%D0%BD%D0%B0%D0%BB%D1%96%D0%B7")]
+        [InlineData("%D0%9A%D0%BE%D0%BC%D0%BF%27%D1%8E%D1%82%D0%B5%D1%80%D0%BD%D1%96%20%D0%BD%D0%B0%D1%83%D0%BA%D0%B8")]
         public async Task GET_EndpointsReturnUniversities_IfSpecialityNameCorrect(string SpecialityName)
         {
             // Act
@@ -87,11 +87,11 @@ namespace YIF_XUnitTests.Integration.YIF_Backend.Controllers
         }
         
         [Theory]
-        [InlineData("%D0%86%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D1%96%D0%B9%D0%BD%D1%96%20%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D1%96%D1%97", "%D0%86%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D1%96%D0%B9%D0%BD%D1%96%20%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B8%20%D1%82%D0%B0%20%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D1%96%D1%97")]
+        [InlineData("%D0%86%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D1%96%D0%B9%D0%BD%D1%96%20%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D1%96%D1%97", "%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%BD%D0%B8%D0%B9%20%D0%B0%D0%BD%D0%B0%D0%BB%D1%96%D0%B7")]
         public async Task GET_EndpointsReturnUniversities_IfDirectionName_And_SpecialityNameCorrect(string DirectionName, string SpecialityName)
         {
             // Act
-            var response = await _client.GetAsync($"?DirectionName={DirectionName}&SpecialityName={SpecialityName}");
+            var response = await _client.GetAsync($"?DirectionName={DirectionName}&SpecialityName={SpecialityName}&page=1&pageSize=10");
             var content = response.Content.ReadAsStringAsync().Result;
 
             var contentJsonObj = JArray.Parse(JObject.Parse(content).GetValue("responseList").ToString());
