4c0342dd4519f227ce576e9e2df0ea6b7c53c5f6
Kolia <kolyalyutko@gmail.com>
2021-01-29 12:20:02 +0200

    Fixed merge tests conflicts

--- YIF.Core.Service/Concrete/Services/UniversityService.cs
+++ YIF.Core.Service/Concrete/Services/UniversityService.cs
@@ -16,7 +16,7 @@ namespace YIF.Core.Service.Concrete.Services
 {
     public class UniversityService : IUniversityService<University>
     {
-        private readonly IUserRepository<DbUser, UserDTO> _userRepository;
+        private readonly IUserRepository<DbUser, UserDTO, UserProfile, UserProfileDTO> _userRepository;
         private readonly IUniversityRepository<University, UniversityDTO> _universityRepository;
         private readonly IRepository<SpecialityToUniversity, SpecialityToUniversityDTO> _specialtyRepository;
         private readonly IRepository<DirectionToUniversity, DirectionToUniversityDTO> _directionRepository;
@@ -25,7 +25,7 @@ namespace YIF.Core.Service.Concrete.Services
         private readonly IPaginationService _paginationService;
 
         public UniversityService(
-            IUserRepository<DbUser, UserDTO> userRepository,
+            IUserRepository<DbUser, UserDTO, UserProfile, UserProfileDTO> userRepository,
             IUniversityRepository<University, UniversityDTO> universityRepository,
             IRepository<SpecialityToUniversity, SpecialityToUniversityDTO> specialtyRepository,
             IRepository<DirectionToUniversity, DirectionToUniversityDTO> directionRepository,
--- YIF_XUnitTests/Unit/YIF.Core.Service/Concrete/Services/UniversityServiceTests.cs
+++ YIF_XUnitTests/Unit/YIF.Core.Service/Concrete/Services/UniversityServiceTests.cs
@@ -24,7 +24,7 @@ namespace YIF_XUnitTests.Unit.YIF.Core.Service.Concrete.Services
         private static readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
         
         private static readonly Mock<IUniversityRepository<University, UniversityDTO>> _universityReposotiry = new Mock<IUniversityRepository<University, UniversityDTO>>();
-        private static readonly Mock<IUserRepository<DbUser, UserDTO>> _userRepository = new Mock<IUserRepository<DbUser, UserDTO>>();
+        private static readonly Mock<IUserRepository<DbUser, UserDTO, UserProfile, UserProfileDTO>> _userRepository = new Mock<IUserRepository<DbUser, UserDTO, UserProfile, UserProfileDTO>>();
         private static readonly Mock<IRepository<SpecialityToUniversity, SpecialityToUniversityDTO>> _specialityRepository = new Mock<IRepository<SpecialityToUniversity, SpecialityToUniversityDTO>>();
         private static readonly Mock<IGraduateRepository<Graduate, GraduateDTO>> _graduateRepository = new Mock<IGraduateRepository<Graduate, GraduateDTO>>();
         private static readonly Mock<IRepository<DirectionToUniversity, DirectionToUniversityDTO>> _directionRepository = new Mock<IRepository<DirectionToUniversity, DirectionToUniversityDTO>>();
