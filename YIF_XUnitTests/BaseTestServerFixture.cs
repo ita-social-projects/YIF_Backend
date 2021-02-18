ae449b21fe40c0681d1c4a4ad641a3908a1692a9
ProfesorProst <profesorprost@gmail.com>
2021-02-18 13:10:10 +0200

    Created base structure for integration tests

---
+++ YIF_XUnitTests/BaseTestServerFixture.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YIF.Core.Data;
+using YIF_Backend;
+
+namespace YIF_XUnitTests
+{
+    public class BaseTestServerFixture
+    {
+        public TestServer TestServer { get; }
+        public EFDbContext DbContext { get; }
+        public HttpClient Client { get; }
+
+        public BaseTestServerFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Testing")
+                .UseStartup<Startup>();
+
+            TestServer = new TestServer(builder);
+            Client = TestServer.CreateClient();
+            DbContext = TestServer.Host.Services.GetService<EFDbContext>();
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            TestServer.Dispose();
+        }
+    }
+}
---
+++ YIF_XUnitTests/Integration/YIF_Backend/Controllers/DirectionControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using YIF.Core.Domain.ApiModels.ResponseApiModels;
+using YIF_Backend;
+using YIF_Backend.Controllers;
+
+namespace YIF_XUnitTests.Integration.YIF_Backend.Controllers
+{
+    public class DirectionControllerTest : IClassFixture<BaseTestServerFixture>
+    {
+        private readonly BaseTestServerFixture _fixture;
+
+        public DirectionControllerTest(BaseTestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+
+        [Fact]
+        public async Task GetAll_EndpointsReturnSuccessAndCorrectContentType()
+        {
+            // Act            
+            var response = await _fixture.Client.GetAsync("api/Specialty/All");
+            response.EnsureSuccessStatusCode();
+            var models = JsonConvert.DeserializeObject<IEnumerable<SpecialtyResponseApiModel>>(await response.Content.ReadAsStringAsync());
+            // Assert
+            Assert.NotEmpty(models);
+        }
+    }
+}
