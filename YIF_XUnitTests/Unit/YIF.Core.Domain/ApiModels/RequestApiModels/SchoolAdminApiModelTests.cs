32b9e46168ebfae7b8b6832b27d957b535f6f17e
Glebhawk <31737653+Glebhawk@users.noreply.github.com>
2021-02-19 12:07:10 +0200

    Added unit tests for SchoolAdmin API models

---
+++ YIF_XUnitTests/Unit/YIF.Core.Domain/ApiModels/RequestApiModels/SchoolAdminApiModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using YIF.Core.Domain.ApiModels.RequestApiModels;
+using YIF.Core.Domain.ApiModels.ResponseApiModels;
+
+namespace YIF_XUnitTests.Unit.YIF.Core.Domain.ApiModels.RequestApiModels
+{
+    public class SchoolAdminApiModelTests
+    {
+        [Theory]
+        [InlineData("SchoolName", "Email", "Password")]
+        [InlineData(null, "Null", "0")]
+        [InlineData("", "", "")]
+        public void Ctor_ShouldImplementParameters(string SchoolName, string Email, string Password)
+        {
+            //Act
+            var actualApiModel = new SchoolAdminApiModel
+            {
+                SchoolName = SchoolName,
+                Email = Email,
+                Password = Password
+            };
+
+            //Assert
+            Assert.Equal(SchoolName, actualApiModel.SchoolName);
+            Assert.Equal(Email, actualApiModel.Email);
+            Assert.Equal(Password, actualApiModel.Password);
+        }
+    }
+}
---
+++ YIF_XUnitTests/Unit/YIF.Core.Domain/ApiModels/ResponseApiModels/EntityApiModels/SchoolAdminResponseApiModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using YIF.Core.Domain.ApiModels.RequestApiModels;
+using YIF.Core.Domain.ApiModels.ResponseApiModels;
+
+namespace YIF_XUnitTests.Unit.YIF.Core.Domain.ApiModels.ResponseApiModels.EntityApiModels
+{
+    public class SchoolAdminResponseApiModelTests
+    {
+        [Theory]
+        [InlineData("Id", "SchoolId", "SchoolName")]
+        [InlineData(null, "Null", "0")]
+        [InlineData("", "", "")]
+        public void Ctor_ShouldImplementParameters(string Id, string SchoolId, string SchoolName)
+        {
+            //Act
+            var actualApiModel = new SchoolAdminResponseApiModel
+            {
+                Id = Id,
+                SchoolId = SchoolId,
+                SchoolName = SchoolName
+            };
+
+            //Assert
+            Assert.Equal(Id, actualApiModel.Id);
+            Assert.Equal(SchoolId, actualApiModel.SchoolId);
+            Assert.Equal(SchoolName, actualApiModel.SchoolName);
+        }
+    }
+}
