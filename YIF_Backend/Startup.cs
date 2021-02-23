8956f742bbced49476229e299573bb4651dbe562
ProfesorProst <profesorprost@gmail.com>
2021-02-23 16:20:22 +0200

    SeederDb only on Testing Environment

--- YIF_Backend/Startup.cs
+++ YIF_Backend/Startup.cs
@@ -213,7 +213,11 @@ namespace YIF_Backend
             #endregion
 
             #region Seeder
-            SeederDB.SeedData(app.ApplicationServices);
+            //SeederDB.SeedData(app.ApplicationServices);
+            if (_currentEnvironment.IsEnvironment("Testing"))
+            {
+                SeederDB.SeedData(app.ApplicationServices);
+            }
             #endregion
 
             #region Swagger
