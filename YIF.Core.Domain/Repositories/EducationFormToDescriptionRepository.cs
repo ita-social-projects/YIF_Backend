62ee5ab0e91c344ee69f1a9bde5432c1b176a794
ProfesorProst <profesorprost@gmail.com>
2021-02-24 11:52:06 +0200

    added repositories for payment and education form

---
+++ YIF.Core.Domain/Repositories/EducationFormToDescriptionRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using YIF.Core.Data.Entities;
+using YIF.Core.Data.Interfaces;
+
+namespace YIF.Core.Domain.Repositories
+{
+    public class EducationFormToDescriptionRepository : IRepository<EducationFormToDescription, EducationFormToDescription>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public EducationFormToDescriptionRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Update(EducationFormToDescription item)
+        {
+            _context.EducationFormToDescriptions.Update(item);
+            var res = await _context.SaveChangesAsync();
+            return res > 0;
+        }
+
+        // Not implemented, as the logic will be determined in the future
+        public Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public Task<IEnumerable<EducationFormToDescription>> Find(Expression<Func<EducationFormToDescription, bool>> predicate)
+        {
+            var list = _context.EducationFormToDescriptions
+                .Include(x => x.SpecialtyInUniversityDescription)
+                .Include(x => x.EducationForm)
+                .Where(predicate)
+                .ToList();
+
+            if (list != null || list.Count > 0)
+            {
+                return Task.FromResult<IEnumerable<EducationFormToDescription>>(list);
+            }
+
+            return null;
+        }
+
+        public async Task<EducationFormToDescription> Get(string id)
+        {
+            var specialtyToUniversity = await _context.EducationFormToDescriptions.FirstOrDefaultAsync(x => x.Id == id);
+            return specialtyToUniversity;
+        }
+
+        // Not implemented, as the logic will be determined in the future
+        public Task<IEnumerable<EducationFormToDescription>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YIF.Core.Domain/Repositories/PaymentFormToDescriptionRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using YIF.Core.Data.Entities;
+using YIF.Core.Data.Interfaces;
+
+namespace YIF.Core.Domain.Repositories
+{
+    public class PaymentFormToDescriptionRepository : IRepository<PaymentFormToDescription, PaymentFormToDescription>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public PaymentFormToDescriptionRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Update(PaymentFormToDescription item)
+        {
+            _context.PaymentFormToDescriptions.Update(item);
+            var res = await _context.SaveChangesAsync();
+            return res > 0;
+        }
+
+        // Not implemented, as the logic will be determined in the future
+        public Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public Task<IEnumerable<PaymentFormToDescription>> Find(Expression<Func<PaymentFormToDescription, bool>> predicate)
+        {
+            var list = _context.PaymentFormToDescriptions
+                .Include(x => x.SpecialtyInUniversityDescription)
+                .Include(x => x.PaymentForm)
+                .Where(predicate)
+                .ToList();
+
+            if (list != null || list.Count > 0)
+            {
+                return Task.FromResult<IEnumerable<PaymentFormToDescription>>(list);
+            }
+
+            return null;
+        }
+
+        public async Task<PaymentFormToDescription> Get(string id)
+        {
+            var paymentFormToDescription = await _context.PaymentFormToDescriptions.FirstOrDefaultAsync(x => x.Id == id);
+            return paymentFormToDescription;
+        }
+
+        // Not implemented, as the logic will be determined in the future
+        public Task<IEnumerable<PaymentFormToDescription>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
