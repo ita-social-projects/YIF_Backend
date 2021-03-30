a30585d7f5cc357137aa6c67d7c5bdd410cd9445
Glebhawk <31737653+Glebhawk@users.noreply.github.com>
2021-03-30 12:07:29 +0300

    Migration completed

---
+++ YIF.Core.Data/Migrations/20210330085740_AddIsDeletedToSpecialtyToInstitutionOfEducation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YIF.Core.Data.Migrations
+{
+    public partial class AddIsDeletedToSpecialtyToInstitutionOfEducation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "SpecialtyToInstitutionOfEducations",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "SpecialtyToInstitutionOfEducations");
+        }
+    }
+}
