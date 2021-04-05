f3d333dc52eea2b4619e8a09a0e23e4fc21ff40b
Nastia Yukhymchuk <yuximchuk2111@gmail.com>
2021-04-05 19:39:58 +0300

    Added comments for Swagger response/request models

--- YIF.Core.Domain/ApiModels/RequestApiModels/InstitutionOfEducationAdminApiModel.cs
+++ YIF.Core.Domain/ApiModels/RequestApiModels/InstitutionOfEducationAdminApiModel.cs
@@ -4,10 +4,18 @@ namespace YIF.Core.Domain.ApiModels.RequestApiModels
 {
     public class InstitutionOfEducationAdminApiModel
     {
+        /// <summary>
+        /// Institution of education Id
+        /// </summary>
+        /// <example>a47e79d8-7e68-43c5-a5c9-1c6dff1e1693</example>
         [Required]
         [StringLength(255)]
         public string InstitutionOfEducationId { get; set; }
 
+        /// <summary>
+        /// Administrator email
+        /// </summary>
+        /// <example>admin@kpi.ua</example>
         [Required]
         [EmailAddress]
         [StringLength(255)]
--- YIF.Core.Domain/ApiModels/ResponseApiModels/SpecialtyToInstitutionOfEducationResponseApiModel.cs
+++ YIF.Core.Domain/ApiModels/ResponseApiModels/SpecialtyToInstitutionOfEducationResponseApiModel.cs
@@ -8,26 +8,32 @@ namespace YIF.Core.Domain.ApiModels.ResponseApiModels
         /// Gets or sets the specialty id to which this specialty to institutionOfEducation belongs.
         /// </summary>
         public string SpecialtyId { get; set; }
+
         /// <summary>
         /// Gets or sets the institutionOfEducation id to which this specialty to institutionOfEducation belongs.
         /// </summary>
         public string InstitutionOfEducationId { get; set; }
+
         /// <summary>
         /// Gets or sets the specialty name to which this specialty to institutionOfEducation belongs.
         /// </summary>
         public string SpecialtyName { get; set; }
+
         /// <summary>
         /// Gets or sets the specialty code to which this specialty to institutionOfEducation belongs.
         /// </summary>
         public string SpecialtyCode { get; set; }
+
         /// <summary>
         /// Gets or sets the institutionOfEducation abbreviation to which this specialty to institutionOfEducation belongs.
         /// </summary>
         public string InstitutionOfEducationAbbreviation { get; set; }
+
         /// <summary>
         /// Field to determine if there is a specialty and institutionOfEducation in favorites
         /// </summary>
         public bool IsFavorite { get; set; }
+
         /// <summary>
         /// Navigation property for the specialty descriptions this specialty to institutionOfEducation belongs to.
         /// </summary>
