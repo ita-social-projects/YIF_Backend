6ba1c531232e83789f065f2df6960675603cb9d4
Nastia Yukhymchuk <yuximchuk2111@gmail.com>
2021-04-09 21:44:34 +0300

    Recreated migration history

---
+++ YIF.Core.Data/Migrations/20210409181117_AddedTypesOfInstitutionsOfEducations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YIF.Core.Data.Migrations
+{
+    public partial class AddedTypesOfInstitutionsOfEducations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstitutionOfEducationType",
+                table: "InstitutionOfEducations",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstitutionOfEducationType",
+                table: "InstitutionOfEducations");
+        }
+    }
+}
