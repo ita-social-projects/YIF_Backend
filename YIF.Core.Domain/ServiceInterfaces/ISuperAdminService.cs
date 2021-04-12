5de9f6cc0c6233f68288a54214b4b7ebc71bc83e
ArturSodolskyi <next.tmz.mit@gmail.com>
2021-04-12 16:55:12 +0300

    Fix mistakes.

--- YIF.Core.Domain/ServiceInterfaces/ISuperAdminService.cs
+++ YIF.Core.Domain/ServiceInterfaces/ISuperAdminService.cs
@@ -8,13 +8,6 @@ namespace YIF.Core.Domain.ServiceInterfaces
 {
     public interface ISuperAdminService
     {
-        /// <summary>
-        /// Add admin if InstitutionOfEducation doesn`t have anyone.
-        /// </summary>
-        /// <param name="InstitutionOfEducationId"></param>
-        /// <param name="AdminEmail">Admins email. If not exist to create</param>
-        /// <param name="request"><see cref="HttpRequest"/> to reset password</param>
-        /// <returns></returns>
         Task<ResponseApiModel<DescriptionResponseApiModel>> AddInstitutionOfEducationAdmin(
             string InstitutionOfEducationId,
             string AdminEmail,
@@ -24,13 +17,6 @@ namespace YIF.Core.Domain.ServiceInterfaces
         Task<ResponseApiModel<DescriptionResponseApiModel>> DisableInstitutionOfEducationAdmin(string id);
         Task<ResponseApiModel<DescriptionResponseApiModel>> DeleteSchoolAdmin(SchoolUniAdminDeleteApiModel schoolUniAdminDeleteApi);
         Task<ResponseApiModel<DescriptionResponseApiModel>> AddInstitutionOfEducationAndAdmin(InstitutionOfEducationCreatePostApiModel schoolUniAdminDeleteApi, HttpRequest request);
-
-        /// <summary>
-        /// Sorting admins 
-        /// </summary>
-        /// <param name="institutionOfEducationAdminFilterModel">Params for sorting</param>
-        /// <param name="pageModel">Params for pagination</param>
-        /// <returns>Sorted list of Institution of Education Admin</returns>
         Task<PageResponseApiModel<InstitutionOfEducationAdminResponseApiModel>> GetAllInstitutionOfEducationAdmins(InstitutionOfEducationAdminSortingModel institutionOfEducationAdminFilterModel, PageApiModel pageModel);
         Task<ResponseApiModel<IEnumerable<SchoolAdminResponseApiModel>>> GetAllSchoolAdmins();
         Task<ResponseApiModel<DescriptionResponseApiModel>> UpdateSpecialtyById(SpecialtyPutApiModel model);
