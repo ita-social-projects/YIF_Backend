6d4073b70ec36105ddfd695043e3a65bd4d01497
stein4444 <eysmont.vitya@gmail.com>
2021-05-10 16:46:52 +0300

    #137 As a IoEModerator, I want to know, who is my boss (IoEAdmin)

--- YIF.Core.Service/Mapping/InstitutionOfEducationAdminMappers.cs
+++ YIF.Core.Service/Mapping/InstitutionOfEducationAdminMappers.cs
@@ -15,7 +15,9 @@ namespace YIF.Core.Service.Mapping
             CreateMap<InstitutionOfEducationDTO, IoEInformationResponseApiModel>();
 
             CreateMap<InstitutionOfEducationDTO, InstitutionOfEducationForInstitutionOfEducationAdminResponseApiModel>();
-
+            CreateMap<InstitutionOfEducationAdminDTO, IoEAdminForIoEModeratorResponseApiModel>()
+                .ForMember(sram => sram.Email, opt => opt.MapFrom(adm => adm.User.Email))
+                .ForMember(sram => sram.Name, opt => opt.MapFrom(adm => adm.User.UserName));
         }
     }
 }
