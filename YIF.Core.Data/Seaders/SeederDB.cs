07929c936a6c88bda93b4d857907975a961a24e4
Greendiax <artlynx0@gmail.com>
2021-06-03 11:11:09 +0300

    Added nessessary using

--- YIF.Core.Data/Seaders/SeederDB.cs
+++ YIF.Core.Data/Seaders/SeederDB.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using YIF.Core.Data.Entities;
 using YIF.Core.Data.Entities.IdentityEntities;
 using YIF.Shared;
