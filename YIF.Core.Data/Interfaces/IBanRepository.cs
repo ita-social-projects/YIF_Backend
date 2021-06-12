f7bf890a7938c7c08e83692126b0a893216e04c7
Greendiax <artlynx0@gmail.com>
2021-06-12 16:52:10 +0300

    Deleted empty line

--- YIF.Core.Data/Interfaces/IBanRepository.cs
+++ YIF.Core.Data/Interfaces/IBanRepository.cs
@@ -3,7 +3,6 @@
 namespace YIF.Core.Data.Interfaces
 {
     public interface IBanRepository
-        
     {
         Task<string> Ban<TEntity>(TEntity entity) where TEntity : class;
         Task<string> Unban<TEntity>(TEntity entity) where TEntity : class;
