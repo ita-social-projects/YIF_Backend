96f42d23d471565300a9ecf96bc0ef430a6d2764
denys <denyshchuk0>
2021-06-13 21:29:19 +0300

    Some fix

--- YIF.Core.Domain/Repositories/SpecialtyRepository.cs
+++ YIF.Core.Domain/Repositories/SpecialtyRepository.cs
@@ -28,11 +28,11 @@ namespace YIF.Core.Domain.Repositories
              return await _context.SaveChangesAsync() > 0;
         }
 
-        public async Task<bool> Delete(string id)
+        public async Task Delete(string id)
         {
             var specialty = _context.Specialties.FirstOrDefault(x => x.Id == id);
             specialty.IsDeleted = true;
-            return await _context.SaveChangesAsync() > 0;
+            await _context.SaveChangesAsync();
         }
 
         public async Task<SpecialtyDTO> Get(string id)
