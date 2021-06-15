6a6ed7acae2e35e6949e36a267e1e5fe2dcb290b
pinkevycdima@gmail.com <pinkevycdima@gmail.com>
2021-06-15 16:03:15 +0300

    Fix 3 bugs

--- YIF.Core.Data/Entities/Lecture.cs
+++ YIF.Core.Data/Entities/Lecture.cs
@@ -10,12 +10,8 @@ namespace YIF.Core.Data.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
         public string InstitutionOfEducationId { get; set; }
-
         [ForeignKey("InstitutionOfEducationId")]
         public InstitutionOfEducation InstitutionOfEducation { get; set; }
-        /// <summary>      р    п8мкуФ6532ГО3Й  РННННННННННННННННННННННННННННННННННННННННННРОООООООООООООООООООООООООООООООООООПЦРфйпп0212ц.
-        /// Link to Identity user
-        /// </summary>
         public DbUser User { get; set; }
     }
 }
--- YIF_Backend/Program.cs
+++ YIF_Backend/Program.cs
@@ -9,7 +9,6 @@ namespace YIF_Backend
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
